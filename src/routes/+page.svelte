64a5bf064385fbb03b55f3727f94ed87ae77e9d6
ishaansk <4ishaan03@gmail.com>
2026-05-20 16:30:32 +0530

    base working text + checkboxes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  let todos = $state([
+    { text: "todo1", done: false },
+    { text: "todo2", done: false },
+  ]);
+</script>
+
+<div class="todos">
+  {#each todos as todo, i}
+    <div class="todo">
+      <input bind:value={todo.text} type="text" />
+      <input bind:checked={todo.done} type="checkbox" />
+    </div>
+  {/each}
+</div>
